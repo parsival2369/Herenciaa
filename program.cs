b4e6aa508633aae68623b53c6351234d275d35d1
parsival2369 <167647797+parsival2369@users.noreply.github.com>
2024-10-23 20:46:34 -0600

    program.cs
    herencia

---
+++ program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioHerencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            {
+                
+                Geometria geometria = new Geometria(5); 
+
+               
+                geometria.Imprimir();
+
+                Console.ReadKey();
+
+
+
+
+            }
+        }
+    }
+}
